598f385c8016e5e6cb4d9146933c9734eccd8d05
Rachael <raelwangui05>
2026-09-25 23:13:11 +0300

    Break test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 console.log("Running tests...");
 const assert = require("assert");
-assert.strictEqual(1 + 1, 2, "Math works");
+assert.strictEqual(1 + 1, 3, "Math works");
 console.log("All tests passed!");
